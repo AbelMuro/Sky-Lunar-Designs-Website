e6b79705fb161e970c5fcf4aa574bd260d3d36d4
abelmuro <abelmuro93@gmail.com>
2022-09-19 21:03:50 -0700

    integrating server in separate file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const url = require('url');
 const fs = require('fs');
 
 http.createServer(function(req, res){
-    if(req.url == "login"){
-        alert("im here");        
+    if(req.url == "/login"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("it works")       
     }
 
 }).listen(1234);
