2acb822d94b7eb6856640d8e4072a930a6b688f5
abelmuro <abelmuro93@gmail.com>
2022-09-20 13:41:54 -0700

    trying to figure out the proxy stuff

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const http = require('http');
+const formidable = require('formidable');
 const url = require('url');
 const fs = require('fs');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 http.createServer(function(req, res){
+
     if(req.url == "/login"){
+        createProxyMiddleware({
+            target: 'http://localhost:5000',
+            changeOrigin: true
+        });
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write("userName " + fields.username);
+            res.write("password" + fields.password)
+            res.end();
+        })   
+    }
+    else{
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("it works")       
+        res.write("it works");
+        res.end()  
     }
 
-}).listen(1234);
+}).listen(5000);
