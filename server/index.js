84567007de83cefc81f88e74c88bce52b66651f0
abelmuro <abelmuro93@gmail.com>
2022-09-20 19:55:19 -0700

    finally fixed the proxy stuff with forms

--- server/index.js
+++ server/index.js
@@ -1,28 +1,17 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const formidable = require('formidable');
-const url = require('url');
-const fs = require('fs');
-const {createProxyMiddleware} = require('http-proxy-middleware');
 
-http.createServer(function(req, res){
 
-    if(req.url == "/login"){
-        createProxyMiddleware({
-            target: 'http://localhost:5000',
-            changeOrigin: true
-        });
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write("userName " + fields.username);
-            res.write("password" + fields.password)
-            res.end();
-        })   
-    }
-    else{
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("it works");
-        res.end()  
-    }
 
-}).listen(5000);
+
+app.use('/login', (req, res) => {
+    var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files){ 
+           res.send(fields.username);
+    })   
+})
+
+app.listen(5000);
+
+
